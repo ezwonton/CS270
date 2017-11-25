915df5492c07a02299131abe60528e54799d159c
ezwonton <eric.wan.97@gmail.com>
2017-11-25 14:02:29 -0500

    week8 edits

--- Week 8 Work/lab8.rkt
+++ Week 8 Work/lab8.rkt
@@ -18,21 +18,44 @@
 (postage45 19)
 (postage45 21)
 
+
+
+
 (define (postage46 x)
   (cond
-    [(<= x 11) (error "Impossible")]
-    [(= x 12) (list 0 2)]
-    [(= x 13) (error "Impossible")]
-    [(= x 14) (list 1 1)]
-    [(= x 15) (error "Impossible")]
+    [(odd? x) (println "Impossible")]
+    [(= x 14) (list 2 1)]
+    [(= x 12) (list 3 0)]
     [(= x 16) (list 1 2)]
-    [(= x 17) (error "Impossible")]
     [else (let (
-               (res (postage45 (- x 4))))
+               (res (postage46 (- x 4))))
                (list (+ 1 (first res)) (second res))
                )]
   )
 )
 
-(postage46 19)
+(postage46 18)
+(postage46 20)
 (postage46 21)
+
+
+
+
+(define (GenDominoes n)
+  (if (> n 0)
+      (append (map (lambda(x) (string-append "V" x)) (GenDominoes (- n 1)))
+              (if (> n 1) (map (lambda(x) (string-append "HH" x)) (GenDominoes (- n 2)))
+              '()
+              )
+      )
+      '("")
+  )
+)
+(println "")
+(GenDominoes 0)
+(GenDominoes 1)
+(GenDominoes 2)
+(GenDominoes 3)
+(GenDominoes 4)
+(GenDominoes 5)
+(GenDominoes 6)
